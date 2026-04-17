13f7b0c13a5f5a76e56f643dfaafeae1cc4b98a2
rtbroadhurst <rtbroadhurst@gmail.com>
2026-04-17 21:54:32 +0100

    Add basic Line class

---
+++ Line.java
@@ -0,0 +1,17 @@
+public class Line {
+    private String name;
+    
+    public Line(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String toString() {
+        return "Line(" + name + ")";
+    }
+}
+

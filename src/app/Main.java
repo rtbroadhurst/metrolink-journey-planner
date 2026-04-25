25b957e10ed6ddf306da8b363e201f39c969e065
rtbroadhurst <rtbroadhurst@gmail.com>
2026-04-25 08:11:25 +0100

    Connect the GUI to Main, and make main run the CLI on cli as an argument.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,33 +1,42 @@
 package app;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.io.IOException;
 
 import cli.Cli;
+import gui.Gui;
 import model.Network;
 import parsing.Parser;
 
 
 /**
  * Entry point for the program.
- * Creates and populates the network then delegates to Cli.
+ * Creates and populates the network then delegates to Gui, or Cli if the user inputs cli as an argument.
  */
 public class Main {
     public static void main(String[] args) {
         Path csv = Paths.get("data/Metrolink_times_linecolour.csv");
         Network network = new Network();
 
+        // Parse the csv.
         try {
             Parser.load(csv, network);
-            Cli cli = new Cli(network);
-            cli.run();
 
         } catch (IOException e) {
             System.err.println("Failed to load CSV: " + e.getMessage());
             System.exit(1);
         }
 
-
+        // Runs the CLI if the user inputs 'cli' as an argument
+        if (args.length > 0 && args[0].equals("cli")) {
+            Cli cli = new Cli(network);
+            cli.run();
+        }  
+        
+        else {
+            Gui gui = new Gui(network);
+            gui.run();
+        }
     }
 }

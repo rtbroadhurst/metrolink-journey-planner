735349b1e9436d3c1f5b7e178a10ab159343bf69
rtbroadhurst <rtbroadhurst@gmail.com>
2026-05-07 22:11:26 +0100

    Improve imports

--- src/parsing/Parser.java
+++ src/parsing/Parser.java
@@ -1,11 +1,11 @@
 package parsing;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.io.IOException;
 import java.util.List;
 
-import model.*;
+import model.Network;
 
 
 /**
